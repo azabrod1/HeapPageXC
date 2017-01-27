0ab045c3fc0ec6844fe32626243ed4adfda3e929
Alex Zabrodskiy <alex@Alexs-MacBook-Pro.local>
2017-01-27 21:23:09 +0000

    Initial Commit

---
+++ HeapPageXC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HeapPageXC
+//
+//  Created by Alex Zabrodskiy on 1/27/17.
+//  Copyright © 2017 Alex Zabrodskiy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
